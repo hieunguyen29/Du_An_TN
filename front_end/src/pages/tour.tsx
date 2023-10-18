cc5851d534f4cd7dfcd27196f450f8059d37f557
hieunguyen29 <105211474+hieunguyen29@users.noreply.github.com>
2023-10-18 19:22:45 +0700

    commmit

--- front_end/src/pages/tour.tsx
+++ front_end/src/pages/tour.tsx
@@ -36,7 +36,7 @@ const TourPage = () => {
           <nav className='p-4 pt-6 pl-20'>
 
             <ul className='flex gap-12'>
-              <a href=""></a>
+              <a href="">Du lịch Tour</a>
               <a href="">Vận chuyển</a>
               <a href="">Tin tức</a>
               <a href="">Khuyến mãi</a>
